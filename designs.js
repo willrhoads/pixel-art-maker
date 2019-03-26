585bdc32ea354f444f609190aef590797fe55b36
willrhoads <will.rhoads@gmail.com>
2019-03-26 17:08:02 -0500

    Added function to paint color on grid

--- designs.js
+++ designs.js
@@ -1,13 +1,25 @@
 // Select color input
 var color = document.getElementById('colorPicker');
+
 // Select size input
 var size = document.getElementById('sizePicker');
-// When size is submitted by the user, call makeGrid()
+
+// Set canvas variable
 var canvas = document.getElementById('pixelCanvas');
+
+// Function to set color of brush
+function colorSelection(){
+  var colorPick = color.value;
+  canvas.style.color = colorPick;
+  return colorPick;
+
+}
+// Function to clear grid for new canvas
 function clear(){
   canvas.innerHTML = "";
 }
 
+// Function to make the grid
 function makeGrid() {
   clear()
   var height = document.getElementById('inputHeight').value;
@@ -20,9 +32,15 @@ function makeGrid() {
       canvas.appendChild(col);
     }
   }
+// Function to change cell color with user's selected color
+  canvas.addEventListener('click', function(){
+    if(event.target && event.target.nodeName == "TD"){
+      event.target.style.backgroundColor = colorSelection();
+    }
+  })
 }
 
-
+// When size is submitted by the user, call makeGrid()
 size.addEventListener('submit', function(event){
   event.preventDefault();
   makeGrid();
