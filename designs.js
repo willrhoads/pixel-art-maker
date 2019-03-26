5b0493a31d4222f8701ed4b843749a2fd6737bc0
willrhoads <will.rhoads@gmail.com>
2019-03-26 15:51:33 -0500

    Create core of function to create grid

--- designs.js
+++ designs.js
@@ -1,11 +1,19 @@
 // Select color input
-var color = document.getElementById('colorPicker';);
+var color = document.getElementById('colorPicker');
 // Select size input
 var height = document.getElementById('inputHeight');
-var width = document.getElementById('inputWidth')
+var width = document.getElementById('inputWidth');
 // When size is submitted by the user, call makeGrid()
-document.addEventListner('submit', makeGrid)
-
 function makeGrid() {
-  
+  var canvas = document.getElementById('pixelCanvas');
+  var height = document.getElementById('inputHeight').value;
+  var width = document.getElementById('inputWidth').value;
+  for var h = 0; h < height; h++){
+    var row = document.createElement('tr');
+    canvas.appendChild(row);
+    for var w = 0; w < width; w++){
+      var col = document.createElement('td');
+      canvas.appendChild(column);
+    }
+  }
 }
