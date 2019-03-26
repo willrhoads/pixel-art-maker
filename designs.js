1d3964b474266997c2dde0b9247bdd46524e9a0a
willrhoads <will.rhoads@gmail.com>
2019-03-26 15:27:34 -0500

    feat: Added event listenters for grid

--- designs.js
+++ designs.js
@@ -1,10 +1,11 @@
 // Select color input
+var color = document.getElementById('colorPicker';);
 // Select size input
-
+var height = document.getElementById('inputHeight');
+var width = document.getElementById('inputWidth')
 // When size is submitted by the user, call makeGrid()
+document.addEventListner('submit', makeGrid)
 
 function makeGrid() {
-
-// Your code goes here!
-
+  
 }
